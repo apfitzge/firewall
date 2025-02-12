82e3d31b8c89c5a2e60745d3cb866d236040c70c
Andrew Fitzgerald <apfitzge@gmail.com>
2025-02-12 21:30:58 +0000

    setup_default for firewall library

--- firewall/src/lib.rs
+++ firewall/src/lib.rs
@@ -1,1 +1,34 @@
+use {
+    aya::{
+        programs::{Xdp, XdpFlags},
+        Ebpf,
+    },
+    aya_log::EbpfLogger,
+};
 
+pub fn setup_default(interface: &str, flags: XdpFlags) -> Result<Ebpf, anyhow::Error> {
+    let mut bpf = load_default_firewall()?;
+    attach_firewall_program(&mut bpf, interface, flags)?;
+    Ok(bpf)
+}
+
+fn load_default_firewall() -> Result<Ebpf, anyhow::Error> {
+    let bpf = Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/firewall"
+    )))?;
+    Ok(bpf)
+}
+
+fn attach_firewall_program(
+    bpf: &mut Ebpf,
+    interface: &str,
+    flags: XdpFlags,
+) -> Result<(), anyhow::Error> {
+    EbpfLogger::init(bpf)?;
+
+    let program: &mut Xdp = bpf.program_mut("firewall").unwrap().try_into()?;
+    program.load()?;
+    program.attach(interface, flags)?;
+    Ok(())
+}
